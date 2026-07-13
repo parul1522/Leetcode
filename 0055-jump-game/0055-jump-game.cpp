e063157e4757d115c5d282da756d45fe32497f40
Parul <parultamaskar01@gmail.com>
2026-07-13 11:59:10 +0530

    Time: 625 ms (6.2%), Space: 53.3 MB (13.61%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n , false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            if (dp[i]) {
+                for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                    dp[i + j] = true;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
